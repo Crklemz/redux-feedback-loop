2e84c76f3ebe15dca48f0b3dfc8e882d72a28d68
Chris Klemz <crklemz@gmail.com>
2021-06-12 15:24:20 -0500

    created routes/paths in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,48 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+//for routes
+import { Route, HashRouter as Router } from "react-router-dom";
+
+//import components
+import Header from '../Header/Header';
+
+
 function App() {
 
   return (
+    <Router>
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
+      <Header />
+
+      <Route path='/' exact>
+          <Feeling />
+      </Route>
+
+      <Route path='/understanding'>
+          <Understanding />
+      </Route>
+
+      <Route path='/support'>
+          <Support />
+      </Route>
+
+      <Route path='/Comment'>
+          <Comment />
+      </Route>
+
+      <Route path='/review'>
+          <Review />
+      </Route>
+
+      <Route path='/admin'>
+          <Admin />
+      </Route>
+      
     </div>
+    </Router>
   );
 }
 
 export default App;
+
