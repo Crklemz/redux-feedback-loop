ee196d2a336a1fc13e2f4b951953b9bd96ee83e0
Chris Klemz <crklemz@gmail.com>
2021-06-13 12:50:46 -0500

    review page prints array results to DOM but prints 4 times in a row

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,30 @@
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 
 function Review() {
 
+    const userReview = useSelector(store => store.feedback)
+
+    const history = useHistory('');
+
     return(
-        <p>
-            Review Page
-        </p>
+        <>
+        <h1>Review your feedback</h1>
+
+        
+
+        <div>
+            {userReview.map((review, i) => (
+                <div key={i}>
+                <p>{review.feeling}</p>
+                <p>{review.understanding}</p>
+                <p>{review.support}</p>
+                <p>{review.comment}</p>
+                </div>
+            ))}
+        </div>
+        </>
     )
 }
 
